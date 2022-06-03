30c62f522e1712e3b6b6740813c597aeee5d15be
Graham Kirwan <graham.kirwan4@gmail.com>
2022-06-02 23:20:26 -0400

    dynamic paths

--- types.ts
+++ types.ts
@@ -31,18 +31,3 @@ export enum Gender {
     Unknown = "unknown",
 }
 
-export interface Location {
-    name: string;
-    url:  string;
-}
-
-export enum Species {
-    Alien = "Alien",
-    Human = "Human",
-}
-
-export enum Status {
-    Alive = "Alive",
-    Dead = "Dead",
-    Unknown = "unknown",
-}
